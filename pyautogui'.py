9bc5993867192e8aadc9a9c2fa3a0828e45d5bbe
Alexander5265 <104996703+Alexander5265@users.noreply.github.com>
2022-05-05 19:31:02 +0500

    Add files via upload

---
+++ pyautogui'.py
@@ -0,0 +1,9 @@
+import pyautogui as bot
+bot.FAILSAFR = False
+bot.KEY_NAMES
+bot.hotkey('win', 's')
+bot.typewrite('google chrome')
+bot.press('enter')
+bot.typewrite('https://googledino.ru/')
+bot.press('enter')
+bot.press('space')
